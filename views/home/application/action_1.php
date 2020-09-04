d1b7b758f965de0cf1ea8fe602b91c92059b5eee
qiwi3268 <37709253+qiwi3268@users.noreply.github.com>
2020-09-04 16:52:23 +0500

    04.09.2020_1

--- views/home/application/action_1.php
+++ views/home/application/action_1.php
@@ -7,14 +7,8 @@
             <span class="header-action__name">ПЕРЕДАТЬ НА РАССМОТРЕНИЕ В ПТО</span>
             <span class="header-action__document">Заявление на экспертизу № 2020-6-1179/2020-8-1025-С от 18.08.2020</span>
             <div class="header-action__buttons">
-                <div class="header-action__button">
-                    <i class="fas fa-check-circle"></i>
-                    <span>ОК</span>
-                </div>
-                <div class="header-action__button">
-                    <i class="fas fa-check-circle"></i>
-                    <span>Отмена</span>
-                </div>
+                <span class="header-action__button">ОК</span>
+                <span class="header-action__button">Отмена</span>
             </div>
         </div>
         <div class="action__row">
@@ -22,20 +16,7 @@
         </div>
     </div>
 
-    <div class="action__header header-action">
-        <div class="header-action__row">
-            <span class="header-action__name">ПЕРЕДАТЬ НА РАССМОТРЕНИЕ В ПТО</span>
-            <span class="header-action__document">Заявление на экспертизу № 2020-6-1179/2020-8-1025-С от 18.08.2020</span>
-            <div class="header-action__buttons">
-                <div class="header-action__button">ОК</div>
-                <div class="header-action__button">Отмена</div>
-            </div>
-        </div>
-        <div class="header-action__row">
-            <span class="header-action__description">Капитальный ремонт здания школы в пос. Увельский, ул. Советская, 38 Увельского муниципального района Челябинской области (смета)Капитальный ремонт здания школы в пос. Увельский, ул. Советская, 38 Увельского муниципального района Челябинской области (смета)Капитальный ремонт здания школы в пос. Увельский, ул. Советская, 38 Увельского муниципального района Челябинской области (смета)</span>
-        </div>
 
-    </div>
     <div class="action__body">d</div>
 
 </div>
