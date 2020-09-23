29b8c4ffe6719f13a574410573d8c377d851d78e
qiwi3268 <37709253+qiwi3268@users.noreply.github.com>
2020-09-23 17:26:50 +0500

    23.09.2020_5

--- views/home/actions/hierarchy_sidebar.php
+++ views/home/actions/hierarchy_sidebar.php
@@ -1,36 +1,78 @@
 
 <?php $variablesTV = \Lib\Singles\VariableTransfer::getInstance(); ?>
 
-
-<div class="application-form__header header-form">
-    <div class="header-form__title">Заявление на экспертизу <?= $variablesTV->getValue('numerical_name') ?></div>
-</div>
-
 <input type="hidden" value="<?= $variablesTV->getValue('id_application') ?>" name="id_application">
 
-<div class="application-form__body">
-    <!--<div class="sidebar-hierarchy">
-        <div class="sidebar-hierarchy__section">
-            <div class="sidebar-hierarchy__item" data-level="0">
-                <i class="sidebar-hierarchy__icon fas fa-caret-right"></i>
-                <div>Заявление</div>
-            </div>
-            <div class="sidebar-hierarchy__item" data-level="1">Договор</div>
-        </div>
-        <div class="sidebar-hierarchy__section">
-            <div class="sidebar-hierarchy__item" data-level="0">Сводное заключение</div>
-        </div>
-        <div class="sidebar-hierarchy__section">
-            <div class="sidebar-hierarchy__item" data-level="0">Заявление</div>
-        </div>
-        <div class="sidebar-hierarchy__section">
-            <div class="sidebar-hierarchy__item" data-level="0">Заявление</div>
-        </div>
-    </div>-->
+<div class="view__object">"Капитальный ремонт по замене системы АПС"в Муниципальном общеобразовательном учреждении «Школа-интернат для детей-сирот и детей, оставшихся без попечения родителей «Семья» г. Магнитогорска, расположенного по адресу: 455026, Челябинская область, г. Магнитогорск, ул. Дружбы, 25»</div>
+
+<div class="view__header">
+    <div class="view__title">Заявление на экспертизу <?= $variablesTV->getValue('numerical_name') ?></div>
+</div>
+
+<div class="view__container">
     
     <div class="view__sidebar">
+        <div class="view__info info">
+            <div class="info__item">
+                <i class="info__icon fas fa-file-alt"></i>
+                <div class="info__description">
+                    <span class="info__label">Стадия</span>
+                    <span class="info__text">Формирование сводного замечания</span>
+                </div>
+            </div>
+            <div class="info__item">
+                <i class="view__icon fas fa-calendar-week"></i>
+                <div class="view__info">
+                    <span class="view__label">Контрольный срок заявления</span>
+                    <span class="view__text">25.10.2020</span>
+                </div>
+            </div>
+            <div class="info__item">
+                <i class="view__icon fas fa-calendar-day"></i>
+                <div class="view__info">
+                    <span class="view__label">Контрольный срок стадии</span>
+                    <span class="view__text">20.09.2020</span>
+                </div>
+            </div>
+            <div class="info__item">
+                <i class="view__icon fas fa-user-edit"></i>
+                <div class="view__info">
+                    <span class="view__label">Ответственные</span>
+                    <span class="view__text">Заявитель</span>
+                </div>
+            </div>
+        </div>
+        
+        <div class="view__hierarchy hierarchy">
+            <?php foreach ($variablesTV->getValue('availableDocuments') as $document): ?>
+                <div class="sidebar-hierarchy__section">
+                    <a href="<?= $document['ref'] ?>" class="sidebar-hierarchy__item" data-depth="<?= $document['depth'] ?>">
+                        <span class="sidebar-hierarchy__name"><?= $document['label'] ?></span>
+                        <?php foreach ($document['descriptions'] as $description): ?>
+                            <span class="sidebar-hierarchy__text"><?= $description ?></span>
+                        <?php endforeach; ?>
+                    </a>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    
+    </div>
+    
+    <div class="view__body">
+    
+    </div>
+    
+    <div class="view__actions">
     
     
+    </div>
+    
+</div>
+
+
+<div class="application-form__body">
+
+    <div class="view__sidebar">
         <div class="view__items">
             <div class="view__item">
                 <i class="view__icon fas fa-file-alt"></i>
