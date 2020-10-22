7ae966a821bffc5c561165e01845ba688d588d3d
qiwi3268 <37709253+qiwi3268@users.noreply.github.com>
2020-10-22 14:55:07 +0500

    22.10.2020_1

--- views/home/modals/comments_group.php
+++ views/home/modals/comments_group.php
@@ -1,9 +1,152 @@
 <div id="group_overlay" class="overlay group-overlay"></div>
 <div id="group_modal" class="modal group-modal">
-    <div class="group-modal__comments">
-        s
+    <div class="group-modal__header">
+        <div class="group-modal__file files filled" data-id_file_field>
+            <div class="files__item" data-read_only="true" data-id="1114" data-state="valid">
+                <div class="files__info">
+                    <i class="files__icon fas fa-file-pdf"></i>
+                    <div class="files__description">
+                        <span class="files__name">Раздел ПД №5 Подраздел №5_АСП.025.2019-ИОС5_ИУЛ.pdf (1).sig</span>
+                        <span class="files__size">148,97 Кб</span>
+                    </div>
+                </div>
+                <div class="files__state">
+                    <i class="files__state-icon fas fa-pen-alt"></i>
+                    <span class="files__state-text">Не подписано</span>
+                </div>
+                <div class="files__actions">
+                    <i class="files__unload fas fa-angle-double-down"></i>
+                </div>
+            </div>
+        </div>
+        <div class="group-modal__actions">
+            <!--todo переделать классы-->
+            <div class="comment-modal__button" data-replace_file>
+                <span class="comment-modal__label-button">Заменить файл</span>
+                <i class="comment-modal__icon-button fas fa-sync-alt"></i>
+            </div>
+            <div class="comment-modal__button" data-delete_file data-active="false">
+                <span class="comment-modal__label-button">Отменить замену</span>
+                <i class="comment-modal__icon-button fas fa-eraser"></i>
+            </div>
+            <div class="comment-modal__button" data-save_group>
+                <span class="comment-modal__label-button">Сохранить ответ</span>
+                <i class="comment-modal__icon-button fas fa-check"></i>
+            </div>
+            <div class="comment-modal__button" data-close_group>
+                <span class="comment-modal__text">Отмена</span>
+                <i class="comment-modal__icon-button fas fa-times"></i>
+            </div>
+        </div>
     </div>
-    <div class="group-modal__messages">
-        s
+    <div class="group-modal__body">
+        <div class="group-modal__comments">
+            <div class="group-modal__comment">
+            
+            </div>
+        </div>
+        <div class="group-modal__messages">
+            <div class="group-message group-modal__message">
+                <div class="group-message__header">
+                    <div class="group-message__item group-message__date">
+                        <i class="fas fa-clock group-message__icon"></i>
+                        <div class="group-message__text">16.07.20 13:55</div>
+                    </div>
+                    <div class="group-message__item">
+                        <i class="fas fa-user group-message__icon"></i>
+                        <div class="group-message__text">Митусов А.В.</div>
+                    </div>
+                </div>
+                <div class="group-message__body">
+                    <div class="group-message__item">
+                        <div class="group-message__text">Замечание принято.
+                            На основании «Ведомости объемов земляных масс» л. 8 раздела ТП-19.048.361-ПЗУ.ГЧ, а также «Справки об объемах земляных работ и расходе основных строительных материалов2 (см. приложение 2), объем выемки грунта составляет 95 м3, объем использования грунта при обратной засыпке составит 58 м3. Таким образом, объем излишков грунта составит 37,0 м3 (67 т).
+                            См. раздел ТП-19.048.361-ООС л. 21, 22.</div>
+                    </div>
+                    <div class="group-message__item">
+                        <i class="fas fa-pencil-ruler group-message__icon"></i>
+                        <div class="group-message__text">ТП-19.048.361-ООС л. 21, 22.</div>
+                    </div>
+                </div>
+            </div>
+            <div class="group-message group-modal__message">
+                <div class="group-message__header">
+                    <div class="group-message__item">
+                        <i class="fas fa-clock group-message__icon"></i>
+                        <div class="group-message__text">16.07.20 13:55</div>
+                    </div>
+                    <div class="group-message__item">
+                        <i class="fas fa-user group-message__icon"></i>
+                        <div class="group-message__text">Митусов А.В.</div>
+                    </div>
+                </div>
+                <div class="group-message__body">
+                    <div class="group-message__item">
+                        <div class="group-message__text">Принято. В список литературы добавлен
+                            • Технический регламент о безопасности сетей газораспределения и газопотребления, утвержденной постановлением Правительства РФ от 29.10.2010 №870,
+                            • Технический регламент о безопасности зданий и сооружений от 30.12.2009 № 384-ФЗ
+                            Принято. Внесены изменения в раздел Пояснительная записка. Представлены достоверные технические условия №ЮУР:ТУ2-117/19 от 29.11.2019г.
+                            - В текстовой части раздела уточнена информация по техническим условиям (ТП-19.048.361-ИОС6.ТЧ л.4) №ЮУР:ТУ2-117/19 от 29.11.2019г.
+                            Информация в разделе ТП-19.048.361-ПЗ.ТЧ л.7-8 приведены в соответствие принятым проектным решениям раздела ИОС6 с учетом замечаний к технической части раздела.
+                            - В текстовой части раздела представлена информация по условия подключена и границам проектирования (Представлен по договор о подключении (техническом присоединении), границам проектирования). См. приложение А.
+                            - Параметры газопровода в точке подключения приведены в соответствие техническим условиям (ИОС6.ТЧ л.4)
+                            - В текстовой части раздела скорректирована информация: газопровод в точке подключения – надземный, - материал в точке подключения – сталь. (ТП-19.048.361-ИОС6.ТЧ л.4)
+                            - Уточнена информацию по расходу газа (убраны опечатки). 29,81 нм³/час.
+                            - представлена информация по количеству котлов.
+                            ТП-19.048.361-ИОС6.ТЧ л.4
+                            - Представлена информация по конструктиву и инженерно-техническим решениям, используемых в системе газоснабжения.
+                            Наружная прокладка трубопроводов по территории школы до точки подключения согласно ТУ №ЮУР:ТУ2-117/19 от 29.11.2019г выполнена надземная из стальных электросварных труб по ГОСТ 10704-91 ф57.
+                            Общая протяженность наружного газопровода до точки подключения составляет 44м.
+                            В точке подключения устанавливается запорная арматура.
+                            Прокладка газопровода выполняется на металлических опорах под газопровод высотой 2, 200м. Расстояние между опорами принято 4м.
+                            См. ТП-19.048.361-ИОС6.ТЧ л.5
+                            - Представлена схема газопровода с указанием границ и сооружений на газопроводе См. л. 10
+                            - Представлена схема узла в точке присоединения. См. ТП-19.048.361-ИОС6.ТЧ л.11
+                            - Подвод к горелки ф25х2,5 запроектирован из стальной водогазопроводной трубы по ГОСТ 3262-75. (ИОС.ТЧ л.5)
+                            - Информацию по физическим методам контроля привести в соответствие п.10.4 СП 62.13330.2011.
+                            Спецификация
+                            п.1 - место установки в точке присоединения См. ИОС.ГЧ л. 11
+                            П.12 -уточнено количество 8 шт.
+                        
+                            П.13, п.14 обосновано применение, ссылками на нормативные документы.
+                            П.15 уточнен диаметр
+                            П.16 применяется для крепления газопровода внутри помещения. См. ИОС.ГЧ л.6
+                            П.17, п.18 указана Серия 5.905-31.07.
+                            П.19 скорректировано на 3 шт.</div>
+                    </div>
+                    <div class="group-message__item">
+                        <i class="fas fa-pencil-ruler group-message__icon"></i>
+                        <div class="group-message__text">ТП-19.048.361-ООС л. 21, 22.</div>
+                    </div>
+                </div>
+            </div>
+            <div class="group-message group-modal__message">
+                <div class="group-message__header">
+                    <div class="group-message__item">
+                        <i class="fas fa-clock group-message__icon"></i>
+                        <div class="group-message__text">16.07.20 13:55</div>
+                    </div>
+                    <div class="group-message__item">
+                        <i class="fas fa-user group-message__icon"></i>
+                        <div class="group-message__text">Митусов А.В.</div>
+                    </div>
+                </div>
+                <div class="group-message__body">
+                    <div class="group-message__item">
+                        <div class="group-message__text">- Информация в разделе ТП-19.048.361-ПЗ.ТЧ л.7-8 не приведена в соответствие принятым проектным решениям раздела ИОС6 с учетом замечаний к технической части раздела. (ТП-19.048.361-ПЗ.ТЧ Раздел 4. «Сведения о потребности объекта капитального строительства в
+                            топливе, газе, воде и электрической энергии» л.8-9).
+                            - В текстовой части раздела не представлена информация по условия подключена и границам проектирования (отразить информацию по договору о подключении (техническом присоединении), границам проектирования). Приложение А- исходно разрешительный документ для разработки и принятия проектных решений отражаемых в текстовой и графической частях раздела.
+                            - Не представлены сведения об установлении видов и лимитов топлива (раздел 2 ТП-19.048.361-ИОС6.ТЧ л.4).
+                            - На схеме сетей наружного газопровода (л.10 ИОС6) не отражена информация по месту установки и количеству запорной арматуры на газопроводе.
+                            Информацию по физическим методам контроля привести в соответствие п.10.4 СП 62.13330.2011 (л.5 ИОС6.ТЧ)
+                            Спецификация:
+                            П.16 привести в соответствие проектным решениям, уточнить количество с учетом п.18.
+                            П.17 уточнить количество с учетом протяженности
+                            П.23 уточнить диаметр газопровода и указать диаметр опоры</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+    
 </div>
