7af2385466db2d65f0816da271cc305f61b03b39
qiwi3268 <37709253+qiwi3268@users.noreply.github.com>
2020-10-25 18:03:34 +0500

    25.10.2020_1

--- views/js/GeComment.js
+++ views/js/GeComment.js
@@ -27,18 +27,64 @@ class GeComment {
    no_files;
    attached_file;
 
-   constructor() {
+   constructor (data, file = null) {
+
+      Object.assign(this, data.comment_data, {id: null});
+      this.attached_file = file ? parseInt(file.dataset.id) : null;
+
+      this.hash = CommentCreator.hash++;
+      GeComment.comments.set(this.hash, this);
+
+      CommentsTable.getInstance().addComment(this, file);
+   }
+
+   static create (comment_creator) {
+      let comments = [];
+
+      if (comment_creator.marked_files.size > 0) {
+
+         comment_creator.marked_files.marked_files.forEach(file => {
+            new GeComment(comment_creator.comment_data, file);
+         });
+
+      } else {
+         new GeComment(comment_creator.comment_data, null);
+      }
+
+      return comments;
+   }
+
+
+   static edit (comment_creator) {
+
+
+      if (comment_creator.marked_files.size > 0) {
+
+         let iterator = comment_creator.marked_files.entries();
+         let first_file = iterator.next().value;
+         comment.attached_file = first_file[0];
+         this.editTableComment(comment, this.comment_hash, first_file[1]);
+
+         this.marked_files.delete(first_file[0]);
+
+      } else {
+         // console.log('edit_old_comment');
+         comment.attached_file = null;
+         this.editTableComment(comment, this.comment_hash);
+
+      }
+
+      this.marked_files.forEach(file => {
+         // console.log('copy');
+         let comment_copy = Object.assign({}, comment, {file: undefined, id:null});
+         comment_copy.attached_file = parseInt(file.dataset.id);
+         this.addCommentToTable(comment_copy, CommentCreator.hash++, file);
+      });
 
    }
 
-/*   static createFromForm(modal) {
-      let comment = new GeComment();
 
-      let field_inputs = modal.querySelectorAll('[data-field_result]');
-      field_inputs.forEach(input => comment[input.name] = input.value || null);
 
-      return comment;
-   }*/
 
 }
 
--- views/js/modals/CommentCreator.js
+++ views/js/modals/CommentCreator.js
@@ -23,9 +23,9 @@ class CommentCreator {
    criticality_value_input;
    note_input;
 
-   comments_table;
-   table_body;
-   files_container;
+   // comments_table;
+   // table_body;
+   // files_container;
 
    saved_file;
 
@@ -98,32 +98,42 @@ class CommentCreator {
       this.handleFiles();
    }
 
-   save() {
-      let comment_data = new CommentData(this.modal);
+   save () {
+      this.comment_data = new CommentData(this);
 
-      validateBlock(this.modal);
+      let is_valid = this.validate();
 
-      this.validateComment(comment_data);
+      if (is_valid) {
+         this.saveComment();
+      }
+
    }
 
-   validateComment (comment_data) {
+   validate () {
+      let result = false;
 
       validateBlock(this.modal);
 
-      comment_data.criticality_name = this.criticality_name_input.innerHTML;
-
-
       if (
-         !comment_data.text
-         || !comment_data.comment_criticality
-         || (!comment_data.normative_document && comment_data.comment_criticality !== '1')
+         !this.comment_data.text
+         || !this.comment_data.comment_criticality
+         || (!this.comment_data.normative_document && this.comment_data.comment_criticality !== '1')
       ) {
          ErrorModal.open('Ошибка при сохранении замечания', 'Не заполнены обязательные поля');
-      } else if (this.marked_files.size === 0 && comment_data.no_files === null) {
+      } else if (this.marked_files.size === 0 && this.comment_data.no_files === null) {
          ErrorModal.open('Ошибка при сохранении замечания', 'Не отмечены файлы к замечанию');
       } else {
-         this.saveComment(comment);
+         result = true;
+         // this.saveComment(comment_data);
       }
+
+      return result;
+   }
+
+
+   saveComment () {
+      this.is_editing ? GeComment.create(this) : GeComment.edit(this);
+
 
    }
 
@@ -131,23 +141,9 @@ class CommentCreator {
       // let counter = 0;
       // let hash;
 
-      if (this.comment_hash === null) {
+      if (!this.is_editing) {
 
-         if (this.marked_files.size > 0) {
 
-            this.marked_files.forEach(file => {
-               let comment_copy = Object.assign({}, comment, {id: null});
-               comment_copy.attached_file = parseInt(file.dataset.id);
-               // this.addCommentToTable(comment_copy, hash + counter++, file);
-               this.addCommentToTable(comment_copy, CommentCreator.hash++, file);
-            });
-
-         } else {
-            comment.attached_file = null;
-            this.addCommentToTable(comment, CommentCreator.hash++);
-
-            // console.log(comment);
-         }
 
       } else {
 
@@ -183,107 +179,6 @@ class CommentCreator {
    }
 
 
-
-   addCommentToTable (comment, hash, file = null) {
-      this.comments_table.dataset.active = 'true';
-
-      comment.hash = hash;
-
-      GeComment.comments.set(hash, comment);
-
-      let actions = document.createElement('DIV');
-      actions.classList.add('comments-table__actions');
-      actions.setAttribute('data-comment_hash', hash);
-      // data_row.appendChild(actions);
-      this.table_body.appendChild(actions);
-
-      let edit_button = this.createActionButton('edit');
-      edit_button.addEventListener('click', () => CommentCreator.getInstance().open(comment));
-      actions.appendChild(edit_button);
-
-      let delete_button = this.createActionButton('delete');
-      delete_button.addEventListener('click', () => this.showAlert(hash));
-      actions.appendChild(delete_button);
-
-      let text_column = document.createElement('DIV');
-      text_column.classList.add('comments-table__column');
-      text_column.setAttribute('data-comment_text', '');
-      text_column.setAttribute('data-comment_hash', hash);
-      text_column.innerHTML = comment.text;
-      // data_row.appendChild(text_column);
-      this.table_body.appendChild(text_column);
-
-
-      let normative_column = document.createElement('DIV');
-      normative_column.classList.add('comments-table__column');
-      normative_column.setAttribute('data-comment_normative_document', '');
-      normative_column.setAttribute('data-comment_hash', hash);
-      normative_column.innerHTML = comment.normative_document;
-      // data_row.appendChild(normative_column);
-      this.table_body.appendChild(normative_column);
-
-
-      let criticality_column = document.createElement('DIV');
-      criticality_column.classList.add('comments-table__column', 'comments-table__criticality');
-      criticality_column.setAttribute('data-comment_criticality', '');
-      criticality_column.setAttribute('data-comment_hash', hash);
-      criticality_column.innerHTML = comment.criticality_name;
-      // data_row.appendChild(criticality_column);
-      this.table_body.appendChild(criticality_column);
-
-
-      let files_column = document.createElement('DIV');
-      files_column.classList.add('comments-table__column');
-      files_column.setAttribute('data-comment_hash', hash);
-      files_column.setAttribute('data-comment_files', '');
-
-      // data_row.appendChild(files_column);
-      this.table_body.appendChild(files_column);
-
-      // todo добавлять, если появляются файлы на редактировании
-      let files_block = document.createElement('DIV');
-      files_block.classList.add('documentation__files', 'files', 'filled');
-      files_block.setAttribute('data-id_file_field', '');
-      files_block.setAttribute('data-mapping_level_1', this.files_container.dataset.mapping_level_1);
-      files_block.setAttribute('data-mapping_level_2', this.files_container.dataset.mapping_level_2);
-      files_column.appendChild(files_block);
-
-      if (file !== null) {
-
-         files_column.appendChild(files_block);
-
-         //=== save file
-         let file_copy = file.cloneNode(true);
-         file_copy.removeAttribute('style');
-         let checkbox = file_copy.querySelector('.files__checkbox');
-         checkbox.remove();
-
-         let ge_file = new GeFile(file_copy, files_block);
-         ge_file.handleActionButtons();
-
-         files_block.appendChild(file_copy);
-         //=== save file
-
-      }
-
-   }
-
-   addFilesToTable (comment, files_block) {
-      this.marked_files.forEach(file_id => {
-         let file_element = this.files_container.querySelector(`.files__item[data-id="${file_id}"]`);
-         let file_copy = file_element.cloneNode(true);
-         file_copy.removeAttribute('style');
-         let checkbox = file_copy.querySelector('.files__checkbox');
-         checkbox.remove();
-
-         let ge_file = new GeFile(file_copy, files_block);
-         ge_file.handleActionButtons();
-
-         files_block.appendChild(file_copy);
-      });
-   }
-
-
    editTableComment (comment, hash, file = null) {
 
       comment.hash = hash;
@@ -299,83 +194,20 @@ class CommentCreator {
       let criticality_column = this.comments_table.querySelector(`[data-comment_hash="${hash}"][data-comment_criticality]`);
       criticality_column.innerHTML = comment.criticality_name;
 
-      // todo сделать проверку на совпадение с уже добавленными файлами
       let files_column = this.comments_table.querySelector(`[data-comment_hash="${hash}"][data-comment_files]`);
       let files_block = files_column.querySelector('.files');
       if (files_block) {
          files_block.innerHTML = '';
+      } else {
+         files_column.appendChild(this.createFileBlock());
       }
 
-      //=== save file
       if (file !== null) {
-         let file_copy = file.cloneNode(true);
-         file_copy.removeAttribute('style');
-         let checkbox = file_copy.querySelector('.files__checkbox');
-         checkbox.remove();
-
-         let ge_file = new GeFile(file_copy, files_block);
-         ge_file.handleActionButtons();
-
-         files_block.appendChild(file_copy);
+         this.addFileToTable(file, files_block);
       }
-      //=== save file
-
-   }
-
-   // todo убрать
-   showAlert(comment_hash) {
-      let alert_modal = document.getElementById('alert_modal');
-      let alert_overlay = document.getElementById('alert_overlay');
-
-      alert_modal.classList.add('active');
-      alert_overlay.classList.add('active');
-
-      let delete_comment = () => {
-         this.comments.delete(comment_hash);
-
-
-         let comment_columns = this.comments_table.querySelectorAll(`[data-comment_hash="${comment_hash}"]`);
-         comment_columns.forEach(column => column.remove());
-
-         if (!this.table_body.querySelector('[data-comment_hash]')) {
-            this.comments_table.dataset.active = 'false';
-         }
-
-         alert_modal.classList.remove('active');
-         alert_overlay.classList.remove('active');
-      };
-
-      let confirm_button = document.getElementById('alert_confirm');
-      confirm_button.addEventListener('click', delete_comment, {once: true});
-
-      let cancel_button = document.getElementById('alert_cancel');
-      cancel_button.addEventListener('click', () => {
-         alert_modal.classList.remove('active');
-         alert_overlay.classList.remove('active');
-         confirm_button.removeEventListener('click', delete_comment);
-
-      }, {once: true});
-
    }
 
 
-
-   createActionButton (action_class) {
-      let action = document.createElement('DIV');
-      action.classList.add('comments-table__action', action_class);
-
-      let action_icon = document.createElement('I');
-      let icon_class = action_class === 'edit' ? 'fa-pen-alt' : 'fa-times';
-      action_icon.classList.add('comments-table__icon-action', 'fas', icon_class);
-      action.appendChild(action_icon);
-
-      let action_text = document.createElement('SPAN');
-      action_text.classList.add('comments-table__label-action');
-      action_text.innerHTML = action_class === 'edit' ? 'Изменить' : 'Удалить';
-      action.appendChild(action_text);
-
-      return action;
-   }
 
    handleFiles () {
       let files = this.modal.querySelectorAll('.files__item');
@@ -476,11 +308,10 @@ class CommentCreator {
 
       this.setFieldValues(comment);
 
-
-
       let criticality_field = this.criticality_name_input.closest('.field');
       if (comment !== null) {
 
+         this.is_editing = true;
          criticality_field.classList.add('filled');
          if (comment.attached_file) {
             let file = this.modal.querySelector(`.files__item[data-id="${comment.attached_file}"]`);
@@ -490,78 +321,183 @@ class CommentCreator {
 
       } else {
          criticality_field.classList.remove('filled');
+         this.is_editing = false;
       }
+   }
 
-      if (comment) {
+   // todo убрать в отдельный модуль
+   showAlert(comment_hash) {
+      let alert_modal = document.getElementById('alert_modal');
+      let alert_overlay = document.getElementById('alert_overlay');
 
+      alert_modal.classList.add('active');
+      alert_overlay.classList.add('active');
 
-         this.comment_hash = hash;
+      let delete_comment = () => {
+         this.comments.delete(comment_hash);
 
-         let comment = this.comments.get(this.comment_hash);
+         let comment_columns = this.comments_table.querySelectorAll(`[data-comment_hash="${comment_hash}"]`);
+         comment_columns.forEach(column => column.remove());
 
-         // console.log(comment);
-         // console.log(this.comment_hash);
-
-         this.id_input.value = comment.id;
-         this.text_input.value = comment.text;
-         this.normative_document_input.value = comment.normative_document;
-
-         if (comment.no_files === null) {
-            this.no_files_checkbox.dataset.selected = 'false';
-            checkbox_icon.classList.add('fa-square');
-            checkbox_icon.classList.remove('fa-check-square');
-            checkbox_input.value = null;
-         } else {
-            this.no_files_checkbox.dataset.selected = 'true';
-            checkbox_icon.classList.add('fa-check-square');
-            checkbox_icon.classList.remove('fa-square');
-            checkbox_input.value = "1";
+         if (!this.table_body.querySelector('[data-comment_hash]')) {
+            this.comments_table.dataset.active = 'false';
          }
 
-         this.criticality_name_input.innerHTML = comment.criticality_name;
-         this.criticality_value_input.value = comment.comment_criticality;
-         criticality_field.classList.add('filled');
+         alert_modal.classList.remove('active');
+         alert_overlay.classList.remove('active');
+      };
 
-         if (this.criticality_value_input.value !== '1') {
-            normative_block.dataset.active = 'true';
-         } else {
-            normative_block.dataset.active = 'false';
-         }
+      let confirm_button = document.getElementById('alert_confirm');
+      confirm_button.addEventListener('click', delete_comment, {once: true});
 
-         // let marked_files = comment.file;
+      let cancel_button = document.getElementById('alert_cancel');
+      cancel_button.addEventListener('click', () => {
+         alert_modal.classList.remove('active');
+         alert_overlay.classList.remove('active');
+         confirm_button.removeEventListener('click', delete_comment);
 
-
-         // console.log(comment);
-
-         this.note_input.value = comment.note;
-
-      } else {
-         this.comment_hash = null;
-
-         this.id_input.value = null;
-         this.text_input.value = '';
-         this.normative_document_input.value = '';
-         normative_block.dataset.active = 'false';
-
-         this.criticality_name_input.innerHTML = 'Выберите критичность';
-         this.criticality_value_input.value = '';
-         criticality_field.classList.remove('filled');
-
-         this.no_files_checkbox.dataset.selected = 'false';
-         checkbox_icon.classList.add('fa-square');
-         checkbox_icon.classList.remove('fa-check-square');
-         checkbox_input.value = null;
-
-         this.note_input.value = '';
-      }
-
+      }, {once: true});
 
    }
 
 }
 
-function CommentData(modal) {
-   let field_inputs = modal.querySelectorAll('[data-field_result]');
+class CommentsTable {
+   element;
+   body;
+   files_container;
+
+   comment_creator;
+
+   static get instance() {
+      return this._instance;
+   }
+
+   static set instance(instance) {
+      this._instance = instance;
+   }
+
+   static getInstance () {
+
+      if (!this.instance) {
+         this.instance = new CommentCreator();
+      }
+
+      return this.instance;
+   }
+
+   constructor () {
+      this.comment_creator = CommentCreator.getInstance();
+
+      this.element = document.getElementById('comments_table');
+      this.body = document.getElementById('comments_table_body');
+      this.files_container = document.getElementById('documentation');
+
+      this.comment_creator = CommentCreator.getInstance();
+
+      this.element.dataset.active = 'true';
+   }
+
+   addComment (comment, file = null) {
+      this.createActions(comment);
+
+      let text_column = document.createElement('DIV');
+      text_column.classList.add('comments-table__column');
+      text_column.setAttribute('data-comment_text', '');
+      text_column.setAttribute('data-comment_hash', comment.hash);
+      text_column.innerHTML = comment.text;
+      // data_row.appendChild(text_column);
+      this.body.appendChild(text_column);
+
+      let normative_column = document.createElement('DIV');
+      normative_column.classList.add('comments-table__column');
+      normative_column.setAttribute('data-comment_normative_document', '');
+      normative_column.setAttribute('data-comment_hash', comment.hash);
+      normative_column.innerHTML = comment.normative_document;
+      // data_row.appendChild(normative_column);
+      this.body.appendChild(normative_column);
+
+      let criticality_column = document.createElement('DIV');
+      criticality_column.classList.add('comments-table__column', 'comments-table__criticality');
+      criticality_column.setAttribute('data-comment_criticality', '');
+      criticality_column.setAttribute('data-comment_hash', comment.hash);
+      criticality_column.innerHTML = comment.criticality_name;
+      // data_row.appendChild(criticality_column);
+      this.body.appendChild(criticality_column);
+
+      if (file !== null) {
+         let files_block = this.createFileBlock(comment);
+         this.addFile(file, files_block);
+      }
+   }
+
+   addFile (file, files_block) {
+      let file_copy = file.cloneNode(true);
+      file_copy.removeAttribute('style');
+      let checkbox = file_copy.querySelector('.files__checkbox');
+      checkbox.remove();
+      let ge_file = new GeFile(file_copy, files_block);
+      ge_file.handleActionButtons();
+      files_block.appendChild(file_copy);
+   }
+
+   createFileBlock (comment) {
+      let files_column = document.createElement('DIV');
+      files_column.classList.add('comments-table__column');
+      files_column.setAttribute('data-comment_hash', comment.hash);
+      files_column.setAttribute('data-comment_files', '');
+
+      let files_block = document.createElement('DIV');
+      files_block.classList.add('documentation__files', 'files', 'filled');
+      files_block.setAttribute('data-id_file_field', '');
+      files_block.setAttribute('data-mapping_level_1', this.files_container.dataset.mapping_level_1);
+      files_block.setAttribute('data-mapping_level_2', this.files_container.dataset.mapping_level_2);
+
+      files_column.appendChild(files_block);
+      this.body.appendChild(files_column);
+
+      return files_block;
+   }
+
+   createActions (comment) {
+      let actions = document.createElement('DIV');
+      actions.classList.add('comments-table__actions');
+      actions.setAttribute('data-comment_hash', comment.hash);
+      this.body.appendChild(actions);
+
+      let edit_button = this.createActionButton('edit');
+      edit_button.addEventListener('click', () => this.comment_creator.open(comment));
+      actions.appendChild(edit_button);
+
+      let delete_button = this.createActionButton('delete');
+      delete_button.addEventListener('click', () => this.comment_creator.showAlert(comment.hash));
+      actions.appendChild(delete_button);
+   }
+
+   createActionButton (action_class) {
+      let action = document.createElement('DIV');
+      action.classList.add('comments-table__action', action_class);
+
+      let action_icon = document.createElement('I');
+      let icon_class = action_class === 'edit' ? 'fa-pen-alt' : 'fa-times';
+      action_icon.classList.add('comments-table__icon-action', 'fas', icon_class);
+      action.appendChild(action_icon);
+
+      let action_text = document.createElement('SPAN');
+      action_text.classList.add('comments-table__label-action');
+      action_text.innerHTML = action_class === 'edit' ? 'Изменить' : 'Удалить';
+      action.appendChild(action_text);
+
+      return action;
+   }
+
+}
+
+
+function CommentData(comment_creator) {
+   let field_inputs = comment_creator.modal.querySelectorAll('[data-field_result]');
    field_inputs.forEach(input => this[input.name] = input.value || null);
+   this.criticality_name = comment_creator.criticality_name_input.innerHTML;
    return this;
 }
+
